a61ae90477f41a7a965ca89523c33cfb028f1e59
namlem4u <68719961+namlem4u@users.noreply.github.com>
2021-06-14 09:01:34 +0700

    Bootstrap 4

---
+++ main.js
@@ -0,0 +1,16 @@
+
+var button1 = document.getElementById("btn1");
+button1.onclick = function(){
+    alert("Thanh you ^^");
+}
+
+
+
+
+var button2 = document.getElementById("btn2");
+button2.onclick = function(){
+    alert("Đã gửi thành công!!!");
+}
+
+
+
